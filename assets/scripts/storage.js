6aa96da30430582ac0be49ff82d78d924820b0f8
this-is-yosuke <yosuke.kibe@gmail.com>
2024-10-02 17:18:12 -0600

    Updated storage.js
    -created objects for:
    keto post, mediterranean post, and vegetarian post
    -added some logic to set into localStorage

--- assets/scripts/storage.js
+++ assets/scripts/storage.js
@@ -13,10 +13,43 @@ const medSug = document.querySelector("#medSug");
 const vegCal = document.querySelector("#vegCal");
 const vegSug = document.querySelector("#vegSug");
 
+// Creating objects for: Keto, Mediterranean, and Vegetarian cards
+
+function ketEntry(){
+    const ketPost = {
+        calCount: ketCal.value,
+        sugCount: ketSug.value,
+    };
+};
+
+function medEntry(){
+    const medPost = {
+        calCount: medCal.value,
+        sugCount: medSug.value,
+    };
+};
+
+function vegEntry(){
+    const vegPost = {
+        calCount: vegCal.value,
+        sugCount: vegSug.value,
+    };
+};
+
 // setting items to localStorage
 
-function save() {
-
+function save(event) {
+    const element = event.target;
+    if(element.matches("#keto")) {
+        ketEntry();
+        localStorage.setItem("keto", JSON.stringify(ketPost));
+    }else if(element.matches("#med")) {
+        medEntry();
+        localStorage.setItem("mediterranean", JSON.stringify(medPost));
+    }else if(element.matches("veg")) {
+        vegEntry();
+        localStorage.setItem("vegetarian", JSON.stringify(vegPost));
+    };
 };
 
 // retrieving items from localStorage
