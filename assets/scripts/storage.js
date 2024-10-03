06b7c8815cbf7a463f142e07790dc784ef2a6d5e
this-is-yosuke <yosuke.kibe@gmail.com>
2024-10-02 19:08:40 -0600

    Updated the js and html
    currently debugging a null in the js

--- assets/scripts/storage.js
+++ assets/scripts/storage.js
@@ -1,11 +1,11 @@
 // This is the scripting for setting and retrieveing from localStorage!
 /* The sample card Shane coded has two inputs: Calories and sugar. Thus, since there are three diets, I will
-   need to retrieve input data six times. At this time there is only one card with two inputs, neither of which
-   has an id. */
+need to retrieve input data six times. At this time there is only one card with two inputs, neither of which
+has an id. */
 
 // Setting input variables
 /* The diets are: keto, mediterranean, and vegetarian, abbreviated to ket, med, and veg respectively. 
-   The inputs are for calories and sugar, abbreviated to cal and sug respectively. */
+The inputs are for calories and sugar, abbreviated to cal and sug respectively. */
 const ketCal = document.querySelector("#ketCal");
 const ketSug = document.querySelector("#ketSug");
 const medCal = document.querySelector("#medCal");
@@ -20,6 +20,7 @@ function ketEntry(){
         calCount: ketCal.value,
         sugCount: ketSug.value,
     };
+    return ketPost;
 };
 
 function medEntry(){
@@ -27,6 +28,7 @@ function medEntry(){
         calCount: medCal.value,
         sugCount: medSug.value,
     };
+    return medPost;
 };
 
 function vegEntry(){
@@ -34,12 +36,15 @@ function vegEntry(){
         calCount: vegCal.value,
         sugCount: vegSug.value,
     };
+    return vegPost;
 };
 
 // setting items to localStorage
 
-function save(event) {
+function savePost(event) {
+    event.preventDefault();
     const element = event.target;
+    console.log("Yhis is the save enet");
     if(element.matches("#keto")) {
         ketEntry();
         localStorage.setItem("keto", JSON.stringify(ketPost));
@@ -47,21 +52,24 @@ function save(event) {
         medEntry();
         localStorage.setItem("mediterranean", JSON.stringify(medPost));
     }else if(element.matches("veg")) {
-        vegEntry();
-        localStorage.setItem("vegetarian", JSON.stringify(vegPost));
-    };
-};
-
-// retrieving items from localStorage
- 
-function retrieve() {
-
-};
-
-// Setting the event listeners
-/* I do not know if there will be a submit button for each card. As it stands, the current card has two inputs
-   and no means of submitting. In case this is, in fact, the desired layout, I will have to trigger the submit
-   when the user clicks off of the card. */
-
-const dietCard = document.querySelector(".card");
-dietCard.addEventListener("blur", save);
+                vegEntry();
+                localStorage.setItem("vegetarian", JSON.stringify(vegPost));
+            };
+        };
+        
+        // retrieving items from localStorage
+        
+        function retrieve() {
+            
+        };
+        
+        // Setting the event listeners
+        /* I do not know if there will be a submit button for each card. As it stands, the current card has two inputs
+        and no means of submitting. In case this is, in fact, the desired layout, I will have to trigger the submit
+        when the user clicks off of the card. */
+        /* Since buttons were added, I'll make the event a click event. I cannot use button as a query Selector,
+        since button is used in the nav. Maybe use a class? */
+        
+        
+        const dietCard = document.querySelector(".enter");
+        dietCard.addEventListener("click", savePost);
